ed98c1990131d3a44eeae42fe072a9c9e91f06b0
Roc° <396635614@qq.com>
2023-12-01 15:44:20 +0800

    Update autoUrl.py

--- autoUrl.py
+++ autoUrl.py
@@ -10,29 +10,29 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 with open('./url.json', 'r', encoding='utf-8') as f:
     urlJson = json.load(f)
 sourceList = ''
-reList = ["https://github.rocs.site/https://raw.githubusercontent.com"]
+proxyUrl = "https://github.rocs.site/https://raw.githubusercontent.com"
 
 if os.path.exists('./tv'):
     shutil.rmtree('./tv')
 for item in urlJson:
     urlReq = requests.get(item["url"], verify=False)
-    for reI in range(len(reList)):
-        urlName = item["name"]
-        urlPath = item["path"]
-        reqText = urlReq.text
-        reqText = reqText.replace("'./", "'" + urlPath) \
-            .replace('"./', '"' + urlPath) \
-            .replace("/raw/", "/") \
-            .replace("'https://github.com", "'" + reList[reI]) \
-            .replace('"https://github.com', '"' + reList[reI]) \
-            .replace("'https://raw.githubusercontent.com", "'" + reList[reI]) \
-            .replace('"https://raw.githubusercontent.com', '"' + reList[reI])
-        sourceList += urlName + '：' + reList[reI] + '/shenpvip/vd/main'+"/tv/" + str(reI) + "/" + urlName + ".json \n\n"
-        filePath = "./tv/" + str(reI)
-        if not os.path.exists(filePath):
-            os.makedirs(filePath)
-        fp = open(filePath + "/" + urlName + ".json", "w+", encoding='utf-8')
-        fp.write(reqText)
+    urlName = item["name"]
+    urlPath = item["path"]
+    reqText = urlReq.text
+    reqText = reqText.replace("'./", "'" + urlPath) \
+        .replace('"./', '"' + urlPath) \
+        .replace("/raw/", "/") \
+        .replace("'https://github.com", "'" + proxyUrl) \
+        .replace('"https://github.com', '"' + proxyUrl) \
+        .replace("'https://raw.githubusercontent.com", "'" + proxyUrl) \
+        .replace('"https://raw.githubusercontent.com', '"' + proxyUrl)
+    sourceList += urlName + '：' + proxyUrl + '/shenpvip/vd/main'+"/tv/" + str(reI) + "/" + urlName + ".json \n\n"
+    filePath = "./tv/" + str(reI)
+    if not os.path.exists(filePath):
+        os.makedirs(filePath)
+    fp = open(filePath + "/" + urlName + ".json", "w+", encoding='utf-8')
+    fp.write(reqText)
+        
 now = datetime.datetime.now()
 fp = open('json.txt', "w+", encoding='utf-8')
 fp.write("本次更新时间为：" + now.strftime("%Y-%m-%d %H:%M:%S") + "\n\n")
